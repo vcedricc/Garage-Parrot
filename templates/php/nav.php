d6d1c7490e18428bbc99ad7c2842225a331160b6
vcedricc <107922337+vcedricc@users.noreply.github.com>
2023-10-18 16:00:10 +0200

    Update UX/UI

--- templates/php/nav.php
+++ templates/php/nav.php
@@ -16,7 +16,7 @@
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="vehicules.php">Nos véhicules</a>
+                        <a id="navbarVehiculesLink" class="nav-link" href="vehicules.php">Nos véhicules</a>
                     </li>
 
                     <li class="nav-item dropdown">
